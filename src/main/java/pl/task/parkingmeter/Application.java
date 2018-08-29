9b065c987781fcff10c97aa7123ab5e7d222bcac
Tomasz Ciecwierz <tomasz.ciecwierz@gmail.com>
2018-08-29 14:40:41 +0200

    del CORS

--- src/main/java/pl/task/parkingmeter/Application.java
+++ src/main/java/pl/task/parkingmeter/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,15 +16,4 @@ public class Application {
 
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedOrigins("http://localhost");
-            }
-        };
-    }
 }
